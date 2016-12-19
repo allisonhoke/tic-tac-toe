872aa36062ab3ca06663da58a3af4969cd0f54ea
Allison Hoke <allisonhoke@gmail.com>
2016-12-19 15:15:19 -0800

    changed board.js into backbone model and adjusted tests to pass

--- src/app/models/board.js
+++ src/app/models/board.js
@@ -1,98 +1,98 @@
-var Board = function() {
-  this.state = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
-};
+import Backbone from 'backbone';
 
-Board.prototype.checkRows = function() {
-  for(var i = 0; i < this.state.length; i++) {
-    var checkMark = this.state[i][0];
-    var counter = 0;
-    for(var j = 0; j < this.state[i].length; j++) {
-      if (this.state[i][j] == checkMark && checkMark !== " "){
-        counter ++;
+const Board = Backbone.Model.extend({
+  defaults: {
+    state: [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+  },
+
+  checkRows: function() {
+    for(var i = 0; i < this.get('state').length; i++) {
+      var checkMark = this.get('state')[i][0];
+      var counter = 0;
+      for(var j = 0; j < this.get('state')[i].length; j++) {
+        if (this.get('state')[i][j] == checkMark && checkMark !== " "){
+          counter ++;
+        }
+      }
+      if (counter == this.get('state').length) {
+        return true;
       }
     }
-    if (counter == this.state.length) {
-      return true;
+    return false;
+  },
+
+  checkColumns: function() {
+    var j = 0;
+    while(j < this.get('state').length) {
+      var i = 0;
+      var checkMark = this.get('state')[i][j];
+      var counter = 0;
+      while(i < this.get('state').length) {
+        if (this.get('state')[i][j] == checkMark && checkMark !== " "){
+          counter++;
+        }
+        i++;
+      }
+      if (counter == this.get('state').length) {
+        return true;
+      }
+      j++;
     }
-  }
-  return false;
-};
+    return false;
+  },
 
-
-Board.prototype.checkColumns = function() {
-
-  var j = 0;
-  while(j < this.state.length) {
+  checkDiags: function() {
     var i = 0;
-    var checkMark = this.state[i][j];
+    var j = 0;
+    var checkMark = this.get('state')[i][j];
     var counter = 0;
-    while(i < this.state.length) {
-      if (this.state[i][j] == checkMark && checkMark !== " "){
+    while(i < this.get('state').length) {
+      if (this.get('state')[i][j] == checkMark && checkMark !== " "){
         counter++;
       }
       i++;
+      j++;
     }
-    if (counter == this.state.length) {
+    if (counter == this.get('state').length) {
       return true;
     }
-    j++;
-  }
-  return false;
-};
-
-
-Board.prototype.checkDiags = function() {
-  var i = 0;
-  var j = 0;
-  var checkMark = this.state[i][j];
-  var counter = 0;
-  while(i < this.state.length) {
-    if (this.state[i][j] == checkMark && checkMark !== " "){
-      counter++;
+    // re-set for other loop
+    i = 0;
+    j = (this.get('state').length - 1);
+    checkMark = this.get('state')[i][j];
+    counter = 0;
+    while(i < this.get('state').length) {
+      if (this.get('state')[i][j] == checkMark && checkMark !== " "){
+        counter++;
+      }
+      i++;
+      j--;
     }
-    i++;
-    j++;
-  }
-  if (counter == this.state.length) {
-    return true;
-  }
-  // re-set for other loop
-  i = 0;
-  j = (this.state.length - 1);
-  checkMark = this.state[i][j];
-  counter = 0;
-  while(i < this.state.length) {
-    if (this.state[i][j] == checkMark && checkMark !== " "){
-      counter++;
+    if (counter == this.get('state').length) {
+      return true;
     }
-    i++;
-    j--;
-  }
-  if (counter == this.state.length) {
-    return true;
-  }
-  return false;
-};
-
-Board.prototype.checkWin = function() {
-  if (this.checkRows() || this.checkColumns() || this.checkDiags()) {
-    return true;
-  } else {
     return false;
+  },
+
+  checkWin: function() {
+    if (this.checkRows() || this.checkColumns() || this.checkDiags()) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  isAvailable: function(i, j) {
+    if (this.get('state')[i][j] === " ") {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  makeMove: function(i, j, marker) {
+    this.get('state')[i][j] = marker;
   }
-};
 
-Board.prototype.isAvailable = function(i, j) {
-  if (this.state[i][j] === " ") {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-Board.prototype.makeMove = function(i, j, marker) {
-  this.state[i][j] = marker;
-};
-
-
+});
 export default Board;
